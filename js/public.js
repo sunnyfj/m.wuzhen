44c278e2477b1ed82e38ba33c568394a183760b3
wangxiaobo <sunnyfj@163.com>
2017-06-14 16:31:15 +0800

    产品模块

--- js/public.js
+++ js/public.js
@@ -8,50 +8,35 @@
 'use strict';
 $(function(){
 	//回到顶部
-	function initTopHoverTree(hvtid, times) {
-		$("#" + hvtid).on("click", function () { goTopHovetree(times); })
+	
+	window.onscroll=function(){
+	    if($(window).scrollTop()>100){
+	         $('#gotop').show();
+	    }else{
+	         $('#gotop').hide();
+	    }
+	} 
+	$('#gotop').click(function(){
+	    scroll('0px', 300);	
+	});
+	function scroll(scrollTo, time) {
+	    var scrollFrom = parseInt($(window).scrollTop()),
+	        i = 0,
+	        runEvery = 5; // run every 5ms
 
-		$(window).scroll(function () {
-			if ($(window).scrollTop() > 268) {
-				$("#" + hvtid).show();
-			}
-			else {
-				$("#" + hvtid).hide();
-			}
-		});
+	    scrollTo = parseInt(scrollTo);
+	    time /= runEvery;
+
+	    var interval = setInterval(function () {
+	        i++;
+
+	        $(window).scrollTop((scrollTo - scrollFrom) / time * i + scrollFrom)
+
+	        if (i >= time) {
+	            clearInterval(interval);
+	        }
+	    }, runEvery);
 	}
-
-	//返回顶部动画
-	//goTop(500);//500ms内滚回顶部
-	function goTopHovetree(times) {
-		if (!!!times) {
-		$(window).scrollTop(0);
-			return;
-		}
-
-	var sh = $('body').scrollTop();//移动总距离
-	var inter = 13.333;//ms,每次移动间隔时间
-	var forCount = Math.ceil(times / inter);//移动次数
-	var stepL = Math.ceil(sh / forCount);//移动步长
-	var timeId = null;
-	function aniHovertree() {
-		!!timeId && clearTimeout(timeId);
-		timeId = null;
-		//console.log($('body').scrollTop());
-		if ($('body').scrollTop() <= 0 || forCount <= 0) {//移动端判断次数好些，因为移动端的scroll事件触发不频繁，有可能检测不到有<=0的情况
-		$('body').scrollTop(0);
-		return;
-		}
-		forCount--;
-		sh -= stepL;
-		$('body').scrollTop(sh);
-		timeId = setTimeout(function () { aniHovertree(); }, inter);
-		}
-		aniHovertree();
-	}
-	initTopHoverTree("gotop",300); 
-
-
 	//侧边导航栏
 	$('#header-btn').click(function(event) {
 		$('body').addClass('sidenav-active');
